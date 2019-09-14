426d65015276e3bfafca89a45dc674a6c7ce44dd
Oskar Randen <oskar.randen@gmail.com>
2019-09-14 15:09:51 +0200

    Added some docstrings and cleaned up the tests

--- exp_decay.py
+++ exp_decay.py
@@ -6,15 +6,26 @@ class ExponentialDecay:
     def __init__(self, a):
         self.a = a
 
-    def model(self, t, u):
+    def __call__(self, t, u):
+        """ Exponential decay function """
         return -self.a * u
 
     def solve(self, u0, T, dt):
+        """ 
+        Solves the ODE
+
+        Parameters:
+        self.model (function): The ODE to be solved
+        u0 (int): The initial value 
+        T (int):the end of the time interval
+        dt (float):The points where the solution is evaluated
+
+        Returns:
+        t (list): Time points
+        u (list): Values of the solution at point t
+        """
         solved = solve_ivp(
-            self.model, [0, T], [u0], method="Radau", t_eval=np.arange(0, T, dt)
+            self, [0, T], [u0], method="Radau", t_eval=np.arange(0, T, dt)
         )
 
         return solved.t, solved.y
-
-    def __call__(self):
-        return self.model
--- test_exp_decay.py
+++ test_exp_decay.py
@@ -1,25 +1,28 @@
 from exp_decay import ExponentialDecay
 from matplotlib import pyplot as plt
+import numpy as np
 
 a = 0.4
-
 u0 = 1
-
-T = 30
-
+T = 15
 dt = 1
 
 decay_model = ExponentialDecay(a)
-
 t, u = decay_model.solve(u0, T, dt)
 
+""" 
+def test_1a():
+    a = 0.4
+    decay_model = ExponentialDecay(a)
+    decay_model(u)
 
-print(t)
 
-print()
+test_1a() """
 
-print(u)
 
-""" plt.plot(t, u)
-plt.show()
- """
+def test_1b():
+    plt.plot(t, u[0, :])
+    plt.show()
+
+
+test_1b()

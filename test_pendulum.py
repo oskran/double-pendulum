35d459156534f6a58aeaacc042e8c5159ad41256
Oskar Randen <oskar.randen@gmail.com>
2019-09-17 15:31:46 +0200

    Removed example

--- test_pendulum.py
+++ test_pendulum.py
@@ -4,20 +4,14 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # a)
-""" def test_2a():
-    omega = 0
-    theta = 0
-    y = [theta, omega]
-
-    f = Pendulum()
-    f() """
 
 
 # b)
 
 # c)
 def test_2c_not_called():
-    """ Test whether an exception is raised if .t, .theta, or .omega are accessed before .solve() is called """
+    """ Test whether an exception is raised if .t, .theta, 
+    or .omega are accessed before .solve() is called """
 
     f = Pendulum()
     try:
@@ -32,8 +26,7 @@ test_2c_not_called()
 
 
 def test_2c_called():
-    """ 
-    Checks that t_i ==  i*dt, and that the .theta and .omega
+    """Checks that t_i ==  i*dt, and that the .theta and .omega
     arrays are full of zeros when the initial condition is [0, 0]
      """
 
@@ -71,47 +64,3 @@ def test_r2eql2():
 
 
 test_r2eql2()
-
-# e)
-# Example use:
-L = 2.7
-omega0 = 0.15
-theta0 = 3.14 / 6
-y0 = [theta0, omega0]
-T = 10
-dt = 0.1
-
-# Create pendulum instance
-f = Pendulum(L=L)
-
-# Solving
-f.solve(y0, T, dt, "rad")
-
-# Plotting the motion
-plt.plot(f.t, f.theta)
-plt.show()
-
-# Plotting the kinetic, potential, and total energy
-plt.plot(f.t, f.potential)  # Potential energy
-plt.plot(f.t, f.kinetic)  # Kinetic energy
-plt.plot(
-    f.t, [f.potential[i] + f.kinetic[i] for i, j in enumerate(f.potential)]
-)  # Total energy
-
-plt.show()
-
-# f)
-# Plot total energy of the dampened pendulum
-B = 0.5  # Dampening term
-f_dampened = DampenedPendulum(L=L, B=B)
-f_dampened.solve(y0, T, dt, "rad")
-
-plt.plot(
-    f_dampened.t,
-    [
-        f_dampened.potential[i] + f_dampened.kinetic[i]
-        for i, j in enumerate(f_dampened.potential)
-    ],
-)
-
-plt.show()

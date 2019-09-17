95858396e1180ac3d19f7e6866a403d1ba3bce73
Oskar Randen <oskar.randen@gmail.com>
2019-09-17 16:15:22 +0200

    Added pendulum example to main block

--- double_pendulum.py
+++ double_pendulum.py
@@ -168,3 +168,37 @@ class DoublePendulum:
 
     def save_animation(self):
         self.animation.save("pendulum_motion.mp4", fps=60)
+
+if __name__ == "__main__":
+    # Parameters:
+    L1 = 1
+    L2 = 1
+    M2 = 1
+    M1 = 1
+    theta1 = 90
+    theta2 = 90
+    omega1 = 0.15
+    omega2 = 0.15
+    y0 = (theta1, theta2, omega1, omega2)
+    T = 10
+    dt = 0.1
+
+    # Create object and solve:
+    f = DoublePendulum(M1=M1, M2=M2, L1=L1, L2=L2)
+    f.solve(y0, T, dt, "deg")
+
+    # 3d) Checking energy conservation - Plottinh
+    # Plots potetial, kinetic and total energy
+    plt.plot(f.t, f.potential)  # Potential energy
+    plt.plot(f.t, f.kinetic)  # Kinetic energy
+    plt.plot(
+        f.t, [f.potential[i] + f.kinetic[i] for i, j in enumerate(f.potential)]
+    )  # Total energy
+
+    plt.show()
+
+    # 4d) Creating an animation
+    # Create animation and save it
+    f.create_animation()
+    f.show_animation()
+    f.save_animation()

442a2e4523466624a46667219f055078b6a71b29
Oskar Randen <oskar.randen@gmail.com>
2019-09-18 00:24:44 +0200

    Changed a name

--- test_exp_decay.py
+++ test_exp_decay.py
@@ -5,10 +5,10 @@ def test_1a():
     """ Tests that the calculated derivative
      of u is right gives some parameter values """
     tol = 1e-15
-    exp = -1.28
+    expected = -1.28
     f = ExponentialDecay(a=0.4)
 
-    calc = abs(f(u=3.2) - exp)
+    calc = abs(f(u=3.2) - expected)
     msg = "Obs: The derivative is not equal to its expected value"
     assert calc < tol, msg
 
